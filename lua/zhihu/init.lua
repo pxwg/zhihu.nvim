e17b23ac0b99a49ecb79087417e9ea39388413f0
Xinyu Xiang <xiangxy23@mails.tsinghua.edu.cn>
2026-02-04 21:33:04 +0800

    update: init function

--- lua/zhihu/init.lua
+++ lua/zhihu/init.lua
@@ -1,7 +1,40 @@
 local M = {}
+local default_opts = {}
 
-function M.setup()
-  require("zhihu.article").create_autocmds()
+---@class ZhihuOpts
+---@field filetypes table<string, ZhihuFiletypeConfig> Mapping of filetypes to their configurations
+
+---@class ZhihuFiletypeConfig
+---@field type '"markdown_to_html"' | '"direct_html"'
+---@field Article? table Optional, required if not using defaults
+---@field converter? fun(content: string): string Required for 'markdown_to_html' type
+---@field direct_converter? fun(content: string): string Required for 'direct_html' type
+
+---Setup zhihu.nvim with optional configuration.
+---
+---# Example
+---```lua
+---{
+---   filetypes = {
+---     ['filetype'] = {
+---       type = 'markdown_to_html' | 'direct_html',
+---       Article = Article class, -- optional, required if not using defaults
+---       converter = function(content) -> string, -- required for 'markdown_to_html' type
+---       direct_converter = function(content) -> html_string, -- required for 'direct_html' type
+---     },
+---     ... -- You can define multiple filetypes
+---   }
+--- }
+--- ```
+---@param opts ZhihuOpts?
+function M.setup(opts)
+  opts = opts or {}
+  default_opts = opts
+  require("zhihu.article").create_autocmds(nil, opts)
+end
+
+function M.get_opts()
+  return default_opts
 end
 
 return M

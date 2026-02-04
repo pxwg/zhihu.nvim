be47de4b3b2356b973e490bf43e09fc2f4b7fa1b
Xinyu Xiang <xiangxy23@mails.tsinghua.edu.cn>
2026-02-04 21:17:26 +0800

    feat: enable configuration of different conversion functions to upload content written in various languages

--- lua/zhihu/article.lua
+++ lua/zhihu/article.lua
@@ -1,15 +1,133 @@
 ---get article according to filetype
 local fs = require 'vim.fs'
 local Get = require 'zhihu.api.article.get'.API
+local html_article = require 'zhihu.article.html'.Article
+local markdown_article = require 'zhihu.article.markdown'.Article
+local md_to_html = require("markdown_to_html").md_to_html
+
 local M = {
   url = Get.url .. '/edit',
-  Articles = {
-    html = require 'zhihu.article.html'.Article,
-    markdown = require 'zhihu.article.markdown'.Article,
-  },
+  Articles = {},
+  _custom_articles = {}, -- custom article configs from opts
+}
+
+-- Initialize with default articles
+M.Articles = {
+  html = html_article,
+  markdown = markdown_article,
 }
 -- default
 M.Articles[0] = M.Articles.markdown
+
+---Register custom article types from user configuration
+---@param opts table? Configuration options with filetypes definition
+function M.register_filetypes(opts)
+  if not opts or not opts.filetypes then
+    return
+  end
+  
+  for filetype, config in pairs(opts.filetypes) do
+    M._custom_articles[filetype] = config
+    M.Articles[filetype] = M._create_article_wrapper(filetype, config)
+  end
+end
+
+---Create an article wrapper based on converter type
+---@param filetype string
+---@param config table {type, Article, converter, direct_converter}
+---@return table Article class wrapper
+function M._create_article_wrapper(filetype, config)
+  local ArticleBase = config.Article or html_article
+  
+  if config.type == "markdown_to_html" then
+    return M._create_markdown_to_html_article(ArticleBase, config.converter)
+  elseif config.type == "direct_html" then
+    return M._create_direct_html_article(ArticleBase, config.direct_converter)
+  else
+    error(("Invalid converter type for filetype '%s': %s"):format(filetype, config.type))
+  end
+end
+
+---Create article class for markdown_to_html conversion
+---@param ArticleBase table base Article class
+---@param converter function(content: string) -> string (markdown to html)
+---@return table Article class
+function M._create_markdown_to_html_article(ArticleBase, converter)
+  if not converter then
+    error("markdown_to_html converter requires a 'converter' function")
+  end
+  
+  local CustomArticle = {
+    __base = ArticleBase,
+  }
+  
+  function CustomArticle:new(article)
+    article = article or {}
+    article = ArticleBase(article)
+    setmetatable(article, {
+      __tostring = self.tostring,
+      __index = self
+    })
+    return article
+  end
+  
+  function CustomArticle:tostring()
+    -- For markdown_to_html, we use base HTML class's tostring
+    return ArticleBase.tostring(self)
+  end
+  
+  setmetatable(CustomArticle, {
+    __index = ArticleBase,
+    __call = CustomArticle.new
+  })
+  
+  function CustomArticle:set_content(content)
+    local md = converter(content)
+    self:set_html(md_to_html(md))
+  end
+  
+  return CustomArticle
+end
+
+---Create article class for direct html conversion
+---@param ArticleBase table base Article class
+---@param direct_converter function(content: string) -> string (direct to html)
+---@return table Article class
+function M._create_direct_html_article(ArticleBase, direct_converter)
+  if not direct_converter then
+    error("direct_html converter requires a 'direct_converter' function")
+  end
+  
+  local CustomArticle = {
+    __base = ArticleBase,
+  }
+  
+  function CustomArticle:new(article)
+    article = article or {}
+    article = ArticleBase(article)
+    setmetatable(article, {
+      __tostring = self.tostring,
+      __index = self
+    })
+    return article
+  end
+  
+  function CustomArticle:tostring()
+    return ArticleBase.tostring(self)
+  end
+  
+  setmetatable(CustomArticle, {
+    __index = ArticleBase,
+    __call = CustomArticle.new
+  })
+  
+  function CustomArticle:set_content(content)
+    local html = direct_converter(content)
+    self:set_html(html)
+  end
+  
+  return CustomArticle
+end
 
 ---convert filename to id
 ---@param filename string?
@@ -89,8 +207,15 @@ end
 
 ---create autocmds
 ---@param augroup_id integer?
-function M.create_autocmds(augroup_id)
+---@param opts table? Configuration options with custom filetypes
+function M.create_autocmds(augroup_id, opts)
   augroup_id = augroup_id or vim.api.nvim_create_augroup("zhihu", {})
+  
+  -- Register custom article types from options
+  if opts then
+    M.register_filetypes(opts)
+  end
+  
   vim.api.nvim_create_autocmd({ "BufReadCmd", "SessionLoadPost" }, {
     pattern = "zhihu://*",
     group = augroup_id,

058c2f8df015a737341e27158a2cd7fcdbac24e3
Wu, Zhenyu <wuzhenyu@ustc.edu>
2026-03-06 00:32:27 +0800

    chore: support typst

--- packages/zfh/lua/zfh/translator/cmd.lua
+++ packages/zfh/lua/zfh/translator/cmd.lua
@@ -1,13 +1,18 @@
 ---use pandoc/typst to convert HTML
 ---@module zfh.translator.cmd
 local CMDTranslator = require 'zfh.translator'.CMDTranslator
+local uv = require 'vim.uv'
+local devnull = "/dev/null"
+if uv.os_uname().sysname == "Windows" then
+  devnull = "null"
+end
 local M = {
   readers = {
     pandoc = CMDTranslator { cmd = "zfh -fhtml -t%s -" },
   },
   writers = {
     pandoc = CMDTranslator { cmd = "zfh -f%s -" },
-    typst = CMDTranslator { cmd = "typst compile --features=html -f html - -" },
+    typst = CMDTranslator { cmd = "typst compile --features=html -f html - - 2> " .. devnull },
   },
 }
 return M

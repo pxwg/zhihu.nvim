b3d16347c4cffd2796ad17f6ea61be58850fed27
Wu, Zhenyu <wuzhenyu@ustc.edu>
2026-06-29 19:15:12 +0800

    feat: support typst

--- packages/zfh/lua/zfh.lua
+++ packages/zfh/lua/zfh.lua
@@ -3,7 +3,7 @@
 local Translator = require 'zfh.translator'.Translator
 local html_to_pandoc = require 'zfh.translator.cmd'.readers.pandoc
 local pandoc_to_html = require 'zfh.translator.cmd'.writers.pandoc
-local typst_to_html = require 'zfh.translator.cmd'.writers.typst
+local compile_text = require 'typst'.compile_text
 local html_to_md = require "zfh.generator.markdown".generator
 local html_to_typst = require "zfh.generator.typst".generator
 local md_to_html = require "markdown_to_html".md_to_html
@@ -24,7 +24,9 @@ local M = {
         return ...
       end,
       markdown = md_to_html,
-      typst = typst_to_html,
+      typst = function(text)
+        return compile_text(text, nil, "html")
+      end,
       _ = pandoc_to_html,
     }
   },
